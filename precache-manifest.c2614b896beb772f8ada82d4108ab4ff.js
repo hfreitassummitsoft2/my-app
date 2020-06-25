d2511f5b8d4da95abfc45560f7d0e68c0a01b037
HelioF <hfreitas@macwareinc.com>
2020-06-25 08:56:05 -0500

    Updates

---
+++ precache-manifest.c2614b896beb772f8ada82d4108ab4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0540c414c119b457350be066fd50ce55",
+    "url": "/hfreitassummitsoft2.github.io/index.html"
+  },
+  {
+    "revision": "5daaec6cc30cc84b15c9",
+    "url": "/hfreitassummitsoft2.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/hfreitassummitsoft2.github.io/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hfreitassummitsoft2.github.io/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5daaec6cc30cc84b15c9",
+    "url": "/hfreitassummitsoft2.github.io/static/js/main.6dbbc32f.chunk.js"
+  },
+  {
+    "revision": "b31be110687d482cd0bb",
+    "url": "/hfreitassummitsoft2.github.io/static/js/runtime-main.d769690a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hfreitassummitsoft2.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
